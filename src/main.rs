5dfe47f953120f19923195a311b3233643046b2e
Michael Abel <mabel@bitmuster.org>
2025-11-15 22:40:48 +0100

    Parse date

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@
 
 https://github.com/tafia/calamine
 https://crates.io/crates/calamine
+https://docs.rs/calamine/latest/calamine/
 
 */
-use calamine::{Reader, Xlsx, open_workbook};
+use calamine::{Data, Reader, Xlsx, open_workbook};
+use chrono::NaiveDate;
 
 fn read() {
     let joker_file = "/home/micha/Repos/SolawiKommisionierSpielplatz/Joker_Solawi-Heckengaeu.xlsx";
@@ -17,6 +19,9 @@ fn read() {
                 "{} {} {} {} {} {} {} {}",
                 row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7]
             );
+            if let Data::DateTime(date) = row[0] {
+                println!("{}", NaiveDate::from(date.as_datetime().unwrap()));
+            }
         }
     }
 }
