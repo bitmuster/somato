de3c53c460c5737cc361de2fd53af30e9b341ff5
Michael Abel <mabel@bitmuster.org>
2025-12-14 12:06:35 +0100

    Add test for real data

--- tests/basic.rs
+++ tests/basic.rs
@@ -1,7 +1,7 @@
 use somajotr::somajotr;
 
 #[test]
-fn basic_load() {
+fn basic_load_synth() {
     let members_file = "/home/micha/Repos/SolawiKommisionierSpielplatz/\
     Entenhausen/\
     members_synthetic.xlsx";
@@ -9,6 +9,22 @@ fn basic_load() {
     Entenhausen/\
     jokers_synthetic.xlsx";
     let tickoff_file = "";
+
+    let result = somajotr::somajotr(members_file, joker_file, tickoff_file);
+    assert!(result.is_ok());
+}
+
+#[test]
+fn basic_load_real() {
+    let members_file = "/home/micha/Repos/SolawiKommisionierSpielplatz/\
+        08_Mitgliederliste/\
+        2023-03-20_Mitgliederliste-Solawi-Heckengaeu_v3_Test_neu_fixed.xlsx";
+    let joker_file = "/home/micha/Repos/SolawiKommisionierSpielplatz/\
+        Daten_Stand_2025.11.27/\
+        Joker_Solawi-Heckengaeu.xlsx";
+    let tickoff_file = "/home/micha/Repos/SolawiKommisionierSpielplatz/\
+        Daten_Stand_2025.11.27/\
+        2024-10-28_Abhaklisten.xlsx";
 
     let result = somajotr::somajotr(members_file, joker_file, tickoff_file);
     assert!(result.is_ok());
